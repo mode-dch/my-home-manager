bdb1695fe781612d1605311a36ef100c37974298
mode-dch <128442751+mode-dch@users.noreply.github.com>
2026-09-07 20:43:26 +1200

    chore: remove auto signature popup

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -5,6 +5,9 @@ vim.pack.add({
 
 require("noice").setup({
 	lsp = {
+		signature = {
+			auto_open = { enabled = false },
+		},
 		override = {
 			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 			["vim.lsp.util.stylize_markdown"] = true,
