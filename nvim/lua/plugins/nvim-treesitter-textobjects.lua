04167d9ae2753319439dce3e7fb82ff230ee078d
mode-dch <128442751+mode-dch@users.noreply.github.com>
2026-09-14 10:55:49 +1200

    fix: remove default ft keymaps from neovim

--- nvim/lua/plugins/nvim-treesitter-textobjects.lua
+++ nvim/lua/plugins/nvim-treesitter-textobjects.lua
@@ -1,5 +1,7 @@
 vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" })
 
+vim.g.no_plugin_maps = true
+
 require("nvim-treesitter-textobjects").setup({
 	move = {
 		set_jumps = true,
