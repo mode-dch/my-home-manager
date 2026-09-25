61e16e487df3033a1231c79e886ea677b48229fc
mode-dch <128442751+mode-dch@users.noreply.github.com>
2026-09-26 02:58:26 +1200

    revert(plugin): remove project.nvim

--- nvim/lua/plugins/project.lua
+++
@@ -1,9 +0,0 @@
-vim.pack.add({
-	"https://github.com/DrKJeff16/project.nvim",
-})
-
-require("project").setup({
-	manual_mode = true,
-})
-
-vim.keymap.set("n", "<leader>cp", "<Cmd>Project root<CR>", { desc = "Set Project Root" })
